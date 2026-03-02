2e0d17c9c3650fbccabe08bb2ee9207008f5c6b4
syedshafinahmed <shafinahmed.cse@gmail.com>
2026-03-02 17:59:48 +0600

    Fixed Banner Layout

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -7,12 +7,12 @@ const Banner = () => {
     <div className='bg-[#F8F8FD] w-full'>
       <div className='max-w-7xl mx-auto px-5 md:px-0'>
         <div className='flex items-center justify-between'>
-          <div>
+          <div className='absolute'>
             <p className='text-5xl md:text-7xl text-[#25324B] clashDisplay font-semibold'>Discover <br /> more than <br /> <span className='text-[#26A4FF]'>5000+ Jobs</span></p>
             <img src={underline} className='mt-3 mb-8' alt="" />
             <p className='text-xl text-[#515B6F] epilogue text-justify'>Great platform for the job seeker that searching for <br /> new career heights and passionate about startups.</p>
 
-            <div className="flex mt-5 max-w-3xl ml-0 epilogue bg-white p-4 border border-gray-300 overflow-hidden shadow-sm">
+            <div className="flex mt-5 relative z-12 w-220 ml-0 epilogue bg-white p-4 border border-gray-300 overflow-hidden shadow-sm">
               <div className="flex items-center flex-1 px-4 py-3 border-r border-gray-300">
                 <FiSearch className="text-gray-400 mr-3 w-5 h-5" />
                 <input
@@ -39,9 +39,9 @@ const Banner = () => {
             </div>
             <p className='epilogue mt-5 text-[#202430]'>Popular : <strong>UI Designer, UX Researcher, Android, Admin</strong></p>
           </div>
-          <div className='relative hidden md:block w-125 h-175'>
-            <img className='absolute top-0 left-0 w-full h-full object-cover z-5' src={pattern} alt="" />
-            <img className='absolute top-0 left-0 w-full h-full object-contain z-10' src={model} alt="" />
+          <div className='relative hidden md:block w-140 h-175'>
+            <img className='absolute top-0 left-200 w-full h-full object-cover z-5' src={pattern} alt="" />
+            <img className='absolute top-0 left-190 w-full h-full object-contain z-10' src={model} alt="" />
           </div>
         </div>
       </div>
